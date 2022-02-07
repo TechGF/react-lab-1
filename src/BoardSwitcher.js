f83a1e8c5e42532597a673ed3bc74841a889e2ee
Gary Feng <garyfeng961@gmail.com>
2022-02-07 00:15:14 -0500

    Fixed Toggle Boards

--- src/BoardSwitcher.js
+++ src/BoardSwitcher.js
@@ -15,10 +15,26 @@ class Board extends React.Component {
 }
 
 class BoardSwitcher extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+    };
+  }
+  handleClick(event) {
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+    if (this.state.clicks > this.props.numBoards - 2) {
+      this.setState({ clicks: 0 });
+    }
+  }
+
+
   render() {
     let boards = [];
     for (let ii = 0; ii < this.props.numBoards; ii++) {
-      let isSelected = ii === 0;
+      let isSelected = ii === this.state.clicks;
       boards.push(
         <Board index={ii} selected={isSelected} key={ii} />
       );
@@ -27,7 +43,7 @@ class BoardSwitcher extends React.Component {
     return (
       <div>
         <div className="boards">{boards}</div>
-        <button>Toggle</button>
+        <button onClick={(e) => this.handleClick(e)}>{this.props.btnText}Toggle</button>
       </div>
     );
   }
